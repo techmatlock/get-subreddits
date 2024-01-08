45f0424bf685a7bea032b7b2cea807a7cbc1579b
Administrator <admin@example.com>
2024-01-08 15:58:21 -0800

    Added get_subreddit_names

--- main.py
+++ main.py
@@ -11,17 +11,27 @@ def get_token():
     client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
     post_data = {"grant_type": "password", "username": USERNAME, "password": PASSWORD}
     headers = {"User-Agent": "get-subreddits v1"}
-    response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+    response = requests.post("https://www.reddit.com/api/v1/access_token", 
+                             auth=client_auth, data=post_data, headers=headers)
     return response.json()["access_token"]
 
+def get_subreddit_names(access_token): 
+    headers = {"Authorization": f"bearer {access_token}", "User-Agent": "get-subreddits v1"}
+    params = {"limit": 100}
+    response = requests.get("https://oauth.reddit.com/subreddits/mine/subscriber", 
+                            headers=headers, params=params)
+    json_data = response.json()
+    
+    all_subreddit_names = []
+    for child in json_data["data"]["children"]:
+        subreddit_name = child["data"]["display_name"] 
+        all_subreddit_names.append(subreddit_name)
+    return all_subreddit_names
 
 def main():
     access_token = get_token()
 
-    headers = {"Authorization": f"bearer {access_token}", "User-Agent": "get-subreddits v1"}
-    response = requests.get("https://oauth.reddit.com/subreddits/mine/subscriber", headers=headers)
-    if response.status_code == 200:
-        print(response.json())
+    get_subreddit_names(access_token)
 
 if __name__ == "__main__":
     main()
